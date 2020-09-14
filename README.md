c21f42783115512454ec6d7e96961c2b57d82ec1
kimminki <alsrl8755@naver.com>
2020-09-14 16:33:33 +0900

    Api Setting - 영화 api를 받아서 axios를 사용하여 설정

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 - [ ] TV Show
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now Playing (Movie)
+- [x] Top Rated (Tv)
+- [x] Popular (Tv, Movie)
+- [x] Upcoming (Movie)
+- [x] Airing Today (Tv)
+- [x] Tv show Detail
+- [x] Movie Detail
+- [x] Search Movies
+- [x] Search Tv
---
+++ src/api.js
@@ -0,0 +1,48 @@
+//  오직 api와 네트워크만 다루는 파일
+import axios from "axios";
+
+//  반복해서 URL을 사용하지 않기 위해 configue 설정
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "1d25c5955966ff38cd4aef02e2e9908f",
+    language: "kr",
+  },
+});
+
+// 작업에 필요한 오브젝트 생성
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  popular: () => api.get("movie/popular"),
+  upcoming: () => api.get("movie/upcoming"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  tvDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
